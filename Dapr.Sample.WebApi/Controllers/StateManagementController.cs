08649c74d54ab0a8d9370e6c6b5fb26c26d867fb
saquibsource <127735009+saquibsource@users.noreply.github.com>
2023-03-27 15:56:16 +0530

    Check In
    Check In

---
+++ Dapr.Sample.WebApi/Controllers/StateManagementController.cs
@@ -0,0 +1,32 @@
+﻿using Dapr.Sample.WebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dapr.Sample.WebApi.Controllers
+{
+    /// <summary>
+    /// Sample showing Dapr State management with controller.
+    /// </summary>
+    [ApiController]
+    public class StateManagementController : ControllerBase
+    {
+        private readonly MyStore _myStore;
+        public StateManagementController(MyStore myStore)
+        {
+            _myStore = myStore;
+        }
+        [HttpGet("myStore/{key}/{value}")]
+        public async Task<IActionResult> Set(string key, string value)
+        {
+            await _myStore.Set(key, value);
+            return Ok();
+        }
+        [HttpGet("myStore/{key}")]
+        public async Task<IActionResult> Get(string key)
+        {
+            
+            var value = await _myStore.Get(key);
+            return Ok(value);
+        }
+    }
+}
---
+++ Dapr.Sample.WebApi/MyStore.cs
@@ -0,0 +1,26 @@
+﻿
+using Dapr;
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+namespace Dapr.Sample.WebApi
+{
+    public class MyStore
+    {
+        private readonly DaprClient _daprClient;
+        public MyStore(DaprClient daprClient)
+        {
+            _daprClient = daprClient;
+        }
+        [HttpPost("myStore/{key}")]
+        public async Task Set(string key, [FromBody] string value)
+        {
+            await _daprClient.SaveStateAsync("mystore", key, value);
+        }
+        [HttpGet("myStore/{key}")]
+        public async Task<string> Get(string key)
+        {
+            return await _daprClient.GetStateAsync<string>("mystore", key);
+        }
+    }
+}
